763c53def0f46e2cd42d75aa38301a9032f43ea9
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-09-01 13:20:39 -0400

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content 
 
     def load_json(self):
-        pass
+        response_list = []
+        for response in json.loads(self.get_response_body()):
+            response_list.append(response)
+        return response_list
